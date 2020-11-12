66d3d5f852c1320e3a9a771fd1d91f3bdb260565
Kalugina Marina <marin.kalugin@gmail.com>
2020-11-12 23:12:24 +0300

    generate objects on level

--- Assets/Scripts/Level/Generate.cs
+++ Assets/Scripts/Level/Generate.cs
@@ -1,68 +1,69 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using static Level.GlobalSettings;
 
 public class Generate : MonoBehaviour
 {
-    private int level = 0;
-    private int countObject = 40;
-
-    private double x1 = -27;
-    private double y1 = 30;
-    private double x2 = 50;
-    private double y2 = -50;
-
     public GameObject stone1Prefab;
     public GameObject stone2Prefab;
     public GameObject grassPrefab;
     public GameObject bushPrefab;
-    System.Random rand = new System.Random();
+
+    private System.Random rand = new System.Random();
+
     float RandX()
     {
-        return (float) (rand.Next() % (x2 - x1) + x1);
+        return (float) ((rand.Next() % (x2 - x1) + x1) * 0.99);
     }
 
     float RandY()
     {
-        return (float) (rand.Next() % (y1 - y2) + y2);
+        return (float) ((rand.Next() % (y1 - y2) + y2) * 0.99);
     }
 
-    // Start is called before the first frame update
+    bool checkPlace(float x, float y)
+    {
+        foreach (var obj in AllObjects)
+        {
+            if ((obj.transform.position.x - x) * (obj.transform.position.x - x) +
+                (obj.transform.position.y - y) * (obj.transform.position.y - y) < 9)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void GenerateObjects(GameObject gameObject)
+    {
+        countObject -= level;
+
+        for (var c = 0; c < countObject / 4; ++c)
+        {
+            bool ok = false;
+            float x = 0, y = 0;
+            while (!ok)
+            {
+                x = RandX();
+                y = RandY();
+                ok = checkPlace(x, y);
+            }
+
+            AllObjects.Add(Instantiate(gameObject, new Vector2(x, y), Quaternion.identity));
+        }
+    }
+
+// Start is called before the first frame update
     void Start()
     {
-        var allObjects = new List<GameObject>();
-
-        countObject -= level;
-        
-        for (var c = 0; c < countObject / 4; ++c)
-        {
-            var x = RandX();
-            var y = RandY();
-            Instantiate(stone1Prefab, new Vector2(x, y), Quaternion.identity);
-        }
-
-        for (var c = 0; c < countObject / 4; ++c)
-        {
-            var x = RandX();
-            var y = RandY();
-            Instantiate(stone2Prefab, new Vector2(x, y), Quaternion.identity);
-        }
-
-        for (var c = 0; c < countObject / 4; ++c)
-        {
-            var x = RandX();
-            var y = RandY();
-            Instantiate(grassPrefab, new Vector2(x, y), Quaternion.identity);
-        }
-
-        for (var c = 0; c < countObject / 4; ++c)
-        {
-            var x = RandX();
-            var y = RandY();
-            Instantiate(bushPrefab, new Vector2(x, y), Quaternion.identity);
-        }
+        GenerateObjects(stone1Prefab);
+        GenerateObjects(stone2Prefab);
+        GenerateObjects(grassPrefab);
+        GenerateObjects(bushPrefab);
     }
 
-    // Update is called once per frame
+// Update is called once per frame
     void Update()
     {
     }
---
+++ Assets/Scripts/Level/GlobalSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level
+{
+    public class GlobalSettings
+    {
+        public  static int level = 0;
+        public  static  int countObject = 40;
+
+        public  static double x1 = -30;
+        public  static double y1 = 30;
+        public  static double x2 = 50;
+        public  static double y2 = -50;
+
+        public static List<GameObject> AllObjects = new List<GameObject>();
+
+    }
+}
