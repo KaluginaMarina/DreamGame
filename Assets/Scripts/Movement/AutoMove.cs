ba343c7c7aac4cbb183f1cd4c872ff93c3a3246c
Kalugina Marina <marin.kalugin@gmail.com>
2020-11-13 20:43:53 +0300

    add bourder for BadGuy

--- Assets/Scripts/Movement/AutoMove.cs
+++ Assets/Scripts/Movement/AutoMove.cs
@@ -1,19 +1,16 @@
 using UnityEngine;
 using static DefaultNamespace.Utils_RandPoint;
+using static Level.GlobalSettings;
 
 [AddComponentMenu("Playground/Movement/Auto Move")]
 [RequireComponent(typeof(Rigidbody2D))]
 public class AutoMove : Physics2DObject
 {
-    // These are the forces that will push the object every frame
-    // don't forget they can be negative too!
     public Vector2 direction = new Vector2(1f, 0f);
+    public GameObject BadGuy;
+    
     private int _frameCount = 50;
     private static System.Random rand = new System.Random();
-
-    //is the push relative or absolute to the world?
-    public bool relativeToRotation = true;
-
 
     // FixedUpdate is called once per frame
     void FixedUpdate()
@@ -24,6 +21,29 @@ public class AutoMove : Physics2DObject
             direction = RandDirection();
         }
 
+        var position = BadGuy.transform.position;
+        if (BadGuy.transform.position.x < x1 + 1.5)
+        {
+            if (direction.x < 0) direction.x = 0;
+            position.Set(x1, position.y, 0);
+        }
+        else if (BadGuy.transform.position.x > x2 - 1.5)
+        {
+            if (direction.x > 0) direction.x = 0;
+            position.Set(x2, position.y, 0);
+        }
+
+        if (BadGuy.transform.position.y > y1)
+        {
+            if (direction.y > 0) direction.y = 0;
+            position.Set(position.x, y1, 0);
+        }
+        else if (BadGuy.transform.position.y < y2)
+        {
+            if (direction.y < 0) direction.y = 0;
+            position.Set(position.x, y2, 0);
+        }
+        
         rigidbody2D.AddForce(direction * 6f);
     }
 
@@ -33,9 +53,7 @@ public class AutoMove : Physics2DObject
     {
         if (this.enabled)
         {
-            float extraAngle = (relativeToRotation) ? transform.rotation.eulerAngles.z : 0f;
-
-            Utils.DrawMoveArrowGizmo(transform.position, direction, extraAngle);
+            Utils.DrawMoveArrowGizmo(transform.position, direction, 0);
         }
     }
 }
