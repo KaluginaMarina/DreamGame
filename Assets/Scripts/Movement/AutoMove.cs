4e8a06905a128127a1b23ce66c64838376dfef56
Kalugina Marina <marin.kalugin@gmail.com>
2020-11-13 20:38:10 +0300

    random moving for BadGuy

--- Assets/Scripts/Movement/AutoMove.cs
+++ Assets/Scripts/Movement/AutoMove.cs
@@ -1,40 +1,41 @@
 using UnityEngine;
-using System.Collections;
+using static DefaultNamespace.Utils_RandPoint;
 
 [AddComponentMenu("Playground/Movement/Auto Move")]
 [RequireComponent(typeof(Rigidbody2D))]
 public class AutoMove : Physics2DObject
 {
-	// These are the forces that will push the object every frame
-	// don't forget they can be negative too!
-	public Vector2 direction = new Vector2(1f, 0f);
+    // These are the forces that will push the object every frame
+    // don't forget they can be negative too!
+    public Vector2 direction = new Vector2(1f, 0f);
+    private int _frameCount = 50;
+    private static System.Random rand = new System.Random();
+
+    //is the push relative or absolute to the world?
+    public bool relativeToRotation = true;
 
 
-	//is the push relative or absolute to the world?
-	public bool relativeToRotation = true;
+    // FixedUpdate is called once per frame
+    void FixedUpdate()
+    {
+        if (_frameCount-- < 0)
+        {
+            _frameCount = rand.Next(50, 200);    
+            direction = RandDirection();
+        }
 
-	
-	// FixedUpdate is called once per frame
-	void FixedUpdate ()
-	{
-		if(relativeToRotation)
-		{
-			rigidbody2D.AddRelativeForce(direction * 2f);
-		}
-		else
-		{
-			rigidbody2D.AddForce(direction * 2f);
-		}
-	}
+        rigidbody2D.AddForce(direction * 6f);
+    }
 
 
-	//Draw an arrow to show the direction in which the object will move
-	void OnDrawGizmosSelected()
-	{
-		if(this.enabled)
-		{
-			float extraAngle = (relativeToRotation) ? transform.rotation.eulerAngles.z : 0f;
-			Utils.DrawMoveArrowGizmo(transform.position, direction, extraAngle);
-		}
-	}
+    //Draw an arrow to show the direction in which the object will move
+    void OnDrawGizmosSelected()
+    {
+        if (this.enabled)
+        {
+            float extraAngle = (relativeToRotation) ? transform.rotation.eulerAngles.z : 0f;
+
+            Utils.DrawMoveArrowGizmo(transform.position, direction, extraAngle);
+        }
+    }
 }
--- Assets/Scripts/Utils/RandPoint.cs
+++ Assets/Scripts/Utils/RandPoint.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using static Level.GlobalSettings;
 
 namespace DefaultNamespace
@@ -6,15 +7,22 @@ namespace DefaultNamespace
     public class Utils_RandPoint
     {
         private static System.Random rand = new System.Random();
-        
+
         public static float RandX()
         {
-            return (float) ((float) rand.Next( (int) x1, (int) x2) * 0.95);
+            return (float) ((float) rand.Next((int) x1, (int) x2) * 0.95);
         }
 
         public static float RandY()
         {
-            return (float) ((float) rand.Next( (int) y2, (int) y1) * 0.95);
+            return (float) ((float) rand.Next((int) y2, (int) y1) * 0.95);
+        }
+
+        public static Vector2 RandDirection()
+        {
+            int randX = rand.Next(0, 15) * (rand.Next(-2, 2) > 0 ? -1 : 1);
+            int randY = (15 - Math.Abs(randX)) * (rand.Next(-2, 2) > 0 ? -1 : 1);
+            return new Vector2(randX, randY);
         }
     }
 }
